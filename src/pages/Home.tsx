cc71990233c8006371f08774136cf6d90ae25699
Igor Ferraz Pereira <igoorferraz23@gmail.com>
2021-08-10 20:52:51 -0300

    applying styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,13 @@
+// Images
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleImage from '../assets/images/google-icon.svg';
 
+// Styles
 import '../styles/auth.scss';
+
+// Components
+import { Button } from '../components/Button';
 
 export function Home() {
     return (
@@ -13,16 +18,16 @@ export function Home() {
                 <p>Tire dúvidas da sua audiencia em tempo-real</p>
             </aside>
             <main>
-                <div className="main-content">
+                <div className="main-container">
                     <img src={logoImg} alt="LetmeAsk" />
-                    <button>
+                    <button className="create-room">
                         <img src={googleImage} alt="Logo do Google" />
                         Crie sua Sala com Google
                     </button>
-                    <div>ou entre em uma sala</div>
+                    <div className="separator">ou entre em uma sala</div>
                     <form>
                         <input type="text" placeholder="Digite o código da sala" />
-                        <button type="submit">Entrar na Sala</button>
+                        <Button  type="submit">Entrar na Sala</Button>
                     </form>
                 </div>
             </main>
