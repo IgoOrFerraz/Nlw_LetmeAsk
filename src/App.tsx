3a1e46748c9b30447a2076f8daffa9631c03a455
Igor Ferraz Pereira <igoorferraz23@gmail.com>
2021-08-14 22:10:17 -0300

    create Room's

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { AuthContextProvider } from "./contexts/AuthContexts";
+import { Room } from "./pages/Room";
 
 //Libraries
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'; // Switch evita que mais de uma Route seja chamada ao mesmo tempo
 
 function App() {
 
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms/new" component={NewRoom} />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -9,13 +9,31 @@ import '../styles/auth.scss';
 import { Button } from '../components/Button';
 
 // Libraries
-import { Link } from 'react-router-dom';
+import { FormEvent, useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { database } from '../services/firebase'; // useAuth ja possui acesso à database, porém por n estar sendo utilizado no objeto denominado, não é possível o acesso
 import { useAuth } from '../hooks/useAuth';
 
 
 export function NewRoom() {
 
     const { user } = useAuth();
+    const history = useHistory();
+    const [newRoom, setNewRoom] = useState('');
+
+    async function handleCreateRoom(event: FormEvent){
+        event.preventDefault(); // Intercepta o evento antes de acontecer, retomando o valor "default", ou seja, não faz nada
+
+        if(newRoom.trim() === ''){ return; }
+
+        const roomRef = database.ref('rooms');
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id
+        });
+
+        history.push(`/rooms/${firebaseRoom.key}`);
+    }
 
     return (
         <div id="page-auth">
@@ -28,8 +46,13 @@ export function NewRoom() {
                 <div className="main-container">
                     <img src={logoImg} alt="LetmeAsk" />
                     <h2>Criar uma nova sala</h2>
-                    <form>
-                        <input type="text" placeholder="Nome da sala" />
+                    <form onSubmit={handleCreateRoom}>
+                        <input 
+                            type = "text" 
+                            placeholder = "Nome da sala" 
+                            onChange = { event => setNewRoom(event.target.value) }
+                            value = { newRoom }
+                        />
                         <Button  type="submit">Criar Sala</Button>
                     </form>
                     <p>Quer entrar em uma sala existente? <Link to="/">clique aqui</Link> </p>
